cfd96154b5d0abf8257452a20fb8ad033aef3828
Gilbert Fine <gilbert.fine@gmail.com>
2019-09-06 22:55:11 +0800

    some new type alias and helper functions

--- Database/Persist/TX/Utils/Esqueleto.hs
+++ Database/Persist/TX/Utils/Esqueleto.hs
@@ -2,12 +2,53 @@ module Database.Persist.TX.Utils.Esqueleto where
 
 -- {{{1
 import           ClassyPrelude                   hiding (delete)
+import qualified Data.List.NonEmpty as LNE
+import           Database.Persist
 import qualified Database.Esqueleto              as E
 import qualified Database.Esqueleto.Internal.Sql as E
 import           Database.Persist.TX.Utils       (unsafeEscapeForSqlLikeT)
 
 import Database.Persist.TX.Utils
 -- }}}1
+
+
+type EsqCondOf a = E.SqlExpr a -> E.SqlExpr (E.Value Bool)
+
+type EsqCondOf2 a b = E.SqlExpr a -> E.SqlExpr b -> E.SqlExpr (E.Value Bool)
+
+type EsqCondOfValue a = E.SqlExpr (E.Value a) -> E.SqlExpr (E.Value Bool)
+
+-- | 过滤某 Entity 表的函数
+type EsqCondOfEntity a = EsqCondOf (Entity a)
+
+type EsqCondOfEntity2 a b = EsqCondOf2 (Entity a) (Entity b)
+
+-- | 过滤某 Entity 表的函数, Maybe 版
+type EsqCondOfMaybeEntity a = EsqCondOf (Maybe (Entity a))
+
+
+-- | Construct a ROW
+esqPgSqlRow2 :: E.SqlExpr (E.Value a) -> E.SqlExpr (E.Value b) -> E.SqlExpr (E.Value (a, b))
+esqPgSqlRow2 x1 x2 = E.unsafeSqlFunction "" (x1, x2)
+
+esqPgSqlRow3 :: E.SqlExpr (E.Value a) -> E.SqlExpr (E.Value b) -> E.SqlExpr (E.Value c) -> E.SqlExpr (E.Value (a, b, c))
+esqPgSqlRow3 x1 x2 x3 = E.unsafeSqlFunction "" (x1, x2, x3)
+
+esqPgSqlRow4 :: E.SqlExpr (E.Value a) -> E.SqlExpr (E.Value b) -> E.SqlExpr (E.Value c) -> E.SqlExpr (E.Value d) -> E.SqlExpr (E.Value (a, b, c, d))
+esqPgSqlRow4 x1 x2 x3 x4 = E.unsafeSqlFunction "" (x1, x2, x3, x4)
+
+
+esqPgSqlArrayOverlap :: E.SqlExpr (E.Value [a]) -> E.SqlExpr (E.Value [b]) -> E.SqlExpr (E.Value Bool)
+esqPgSqlArrayOverlap = E.unsafeSqlBinOp "&&"
+
+
+esqOr :: LNE.NonEmpty (E.SqlExpr (E.Value Bool)) -> E.SqlExpr (E.Value Bool)
+esqOr (x LNE.:| xs) = foldr (E.||.) x xs
+
+
+esqAnd :: LNE.NonEmpty (E.SqlExpr (E.Value Bool)) -> E.SqlExpr (E.Value Bool)
+esqAnd (x LNE.:| xs) = foldr (E.&&.) x xs
+
 
 
 esqPgSqlContainsI :: E.SqlExpr (E.Value Text) -> Text -> E.SqlExpr (E.Value Bool)
