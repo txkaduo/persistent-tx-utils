d8b240f1cb73bda2fc9e1bad7c9d28551096ada7
Gilbert Fine <gilbert.fine@gmail.com>
2019-09-06 22:54:52 +0800

    some instances for UUID

--- Database/Persist/TX/Utils/Orphans.hs
+++ Database/Persist/TX/Utils/Orphans.hs
@@ -2,6 +2,8 @@
 module Database.Persist.TX.Utils.Orphans where
 
 import ClassyPrelude
+import Web.PathPieces
+import Data.UUID as U
 import Database.Persist.Sql
 
 
@@ -19,3 +21,18 @@ from9 (a,b,c,d,e,f,g,h,i) = ((a,b),(c,d),(e,f),(g,h),i)
 to9 :: ((a,b),(c,d),(e,f),(g,h),i) -> (a,b,c,d,e,f,g,h,i)
 to9 ((a,b),(c,d),(e,f),(g,h),i) = (a,b,c,d,e,f,g,h,i)
 
+
+instance PersistFieldSql UUID where
+  sqlType _ = SqlOther "UUID"
+
+instance PersistField UUID where
+  toPersistValue = PersistDbSpecific . U.toASCIIBytes
+
+  fromPersistValue (PersistDbSpecific bs) = maybe (Left "Invalid UUID text") Right $ U.fromASCIIBytes bs
+  fromPersistValue (PersistByteString bs) = maybe (Left "Invalid UUID text") Right $ U.fromASCIIBytes bs
+  fromPersistValue (PersistText t)        = maybe (Left "Invalid UUID text") Right $ U.fromText t
+  fromPersistValue x                      = Left $ "PersistJson must be converted from PersistDbSpecific/PersistByteString/PersistText, but got " <> tshow x
+
+instance PathPiece UUID where
+  toPathPiece = U.toText
+  fromPathPiece = U.fromText
